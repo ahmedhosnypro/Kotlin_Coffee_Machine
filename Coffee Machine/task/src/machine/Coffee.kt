7408f29dc24d23d06952d4ce0b6b9239adca8573
ahmedhosnypro <ahmedhosnypro@gmail.com>
2022-06-03 22:02:16 +0200

    Estimate the number of servings

--- Coffee Machine/task/src/machine/Coffee.kt
+++ Coffee Machine/task/src/machine/Coffee.kt
@@ -1,10 +1,41 @@
 package machine
 
-object Coffee {
-    fun calcIngredients(cups: Int) {
-        println("For 25 cups of coffee you will need:")
-        println((cups * 200).toString() + " ml of water")
-        println((cups * 50).toString() + " ml of milk")
-        println((cups * 15).toString() + " g of coffee beans")
+import java.util.*
+
+open class Coffee {
+    companion object {
+        private var scanner = Scanner(System.`in`)
+
+        fun calcNumberOfServings() {
+            println("Write how many ml of water the coffee machine has:")
+            var milkV = 0
+            var waterV = 0
+            var coffeeM = 0
+            var wantedCups = 0
+            try {
+                waterV = scanner.nextLine().trim { it <= ' ' }.toInt()
+                println("Write how many ml of milk the coffee machine has: ")
+                milkV = scanner.nextLine().trim { it <= ' ' }.toInt()
+                println("Write how many grams of coffee beans the coffee machine has: ")
+                coffeeM = scanner.nextLine().trim { it <= ' ' }.toInt()
+                println("Write how many cups of coffee you will need: ")
+                wantedCups = scanner.nextLine().trim { it <= ' ' }.toInt()
+            } catch (e: IllegalArgumentException) {
+                println("please enter a number")
+            }
+            var cupsAvailable = waterV / 200
+            cupsAvailable = cupsAvailable.coerceAtMost(milkV / 50)
+            cupsAvailable = cupsAvailable.coerceAtMost(coffeeM / 15)
+            if (cupsAvailable == wantedCups) {
+                println("Yes, I can make that amount of coffee\n")
+            } else if (cupsAvailable < wantedCups) {
+                println("No, I can make only $cupsAvailable cup(s) of coffee")
+            } else {
+                println(
+                    "Yes, I can make that amount of coffee (and even " +
+                            (cupsAvailable - wantedCups) + " more than that)"
+                )
+            }
+        }
     }
 }
--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,11 +1,7 @@
 package machine
 
-import machine.Coffee.calcIngredients
-import java.util.*
+import machine.Coffee.Companion.calcNumberOfServings
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    println("Write how many cups of coffee you will need:")
-    val cupsNum = scanner.nextInt()
-    calcIngredients(cupsNum)
+    calcNumberOfServings()
 }
